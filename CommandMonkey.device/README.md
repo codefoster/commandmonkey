46dc4b911e146a3dd0c3bbff5c58265eab7b7f66
Jeremy Foster <jeremy.foster@live.com>
2015-02-09 11:09:45 -0800

    initial commit

---
+++ CommandMonkey.device/README.md
@@ -0,0 +1,3 @@
+﻿# CommandMonkey.device
+
+
---
+++ CommandMonkey.service/app.js
@@ -0,0 +1,28 @@
+﻿var http = require('http');
+var app = require('express')();
+
+var targetSocket;
+
+app.set('port', process.env.PORT);
+app.get('/api/command', function (req, res) {
+    targetSocket.emit('command', req.query.cmd);
+});
+
+module.exports = app;
+
+var server = http.createServer(app).listen(app.get('port'));
+
+var io = require('socket.io')(server);
+
+io.on('connection', function (socket) {
+    console.log('connection from client ' + socket.id);
+    socket.on('setTarget', function () {
+        console.log('Setting ' + socket.id + ' as target...');
+        targetSocket = socket
+    });
+    socket.on('command', function (cmd) {
+        console.log('Received command: ' + cmd);
+        console.log('Sending ' + cmd + ' command to target...');
+        if (targetSocket) targetSocket.emit('command', cmd);
+    });
+});
