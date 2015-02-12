357d4ce5b0a754f92e22f3e4710f463b8a1b5c26
Jeremy Foster <jeremy.foster@live.com>
2015-02-12 07:57:43 -0800

    change .device app.js to work on actual device

--- CommandMonkey.device/app.js
+++ CommandMonkey.device/app.js
@@ -1,8 +1,24 @@
-﻿var socket = require('socket.io-client')
+﻿var cylon = require('cylon');
+var socket = require('socket.io-client')
     .connect('http://CommandMonkey.azurewebsites.net');
 
-console.log("Requesting to be target...");
-socket.emit('setTarget');
-socket.on('command', function (cmd) {
-    console.log('Received a command: ' + cmd);
-})
+//work on a real device
+cylon.robot({
+    name: 'edison',
+    connections: { edison: { adaptor: 'intel-iot' } },
+    devices: { monkey: { driver: 'direct-pin', pin: 2 } },
+    work: function (edison) {
+        socket.emit('setTarget');
+        socket.on('command', function (cmd) {
+            edison.monkey.digitalWrite(1);
+            setTimeout(function () { edison.monkey.digitalWrite(0); }, 2000);
+        })
+    }
+}).start();
+
+////test in a console window
+//console.log("Requesting to be target...");
+//socket.emit('setTarget');
+//socket.on('command', function (cmd) {
+//    console.log('Received a command: ' + cmd);
+//})
